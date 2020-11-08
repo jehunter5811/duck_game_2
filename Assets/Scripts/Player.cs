51b3cb92db57aca4e62c14d251cb95d86ba79123
polluterofminds <justin.edward.hunter@gmail.com>
2020-11-08 08:09:37 -0600

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2;
+
+    private void Update()
+    {
+        // https://docs.unity3d.com/ScriptReference/Input.GetAxisRaw.html
+        float rawHorizontalAxis = Input.GetAxisRaw("Horizontal");
+
+        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Vector3-zero.html
+        Vector3 direction = Vector3.zero;
+        direction.x = rawHorizontalAxis;
+
+        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Time-deltaTime.html
+        float timeSinceLastFrame = Time.deltaTime;
+
+        Vector3 translation = direction * speed * timeSinceLastFrame;
+
+        // https://docs.unity3d.com/2020.1/Documentation/ScriptReference/Transform.Translate.html
+        transform.Translate(
+          translation
+        );
+    }
+}
